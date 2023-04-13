5815975034ddbecd231c857623ffe3e0773cbc07
Lscheon <dl786aa@gmail.com>
2023-04-13 20:36:32 +0900

    명령어 입력

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,35 @@
 import java.util.Scanner;
 
 public class Main {
-public static void main(String[] args) {
-	System.out.println("== 프로그램 시작 ==");
-	
-	
-	Scanner sc = new Scanner(System.in);
-	String cmd = sc.nextLine();
-	System.out.println(cmd);
-	
-	
-	
-	
-	System.out.println("== 프로그램 끝 ==");
-	
-	
-	
-	
-	
-}
+	public static void main(String[] args) {
+		System.out.println("== 프로그램 시작 ==");
+
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		while(true) {
+			System.out.println("명령어)");
+			String cmd = sc.nextLine();
+			
+			if(cmd.equals("exit")) {
+				break;
+			}
+			
+		}
+		
+		sc.close();
+		
+		System.out.println("== 프로그램 끝 ==");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
 }
